9dd3a1ef6052eba1a5da704e45aac32f4cf5911c
anfisbena <republicacoc@gmail.com>
2021-10-25 15:34:22 -0500

    aa

--- users/routes.js
+++ users/routes.js
@@ -7,8 +7,6 @@ const router = express.Router();
 
 router.get("/", views.ListView);
 
-router.post("/new", views.createView);
-
 router.put("/edit", views.editView);
 
 router.delete("/delete", views.deleteView);
