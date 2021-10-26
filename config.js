213f44e089d654cdd1c8404a10749fffd478aadc
juanpabloAB <jp.acosta20@gmail.com>
2021-10-26 18:53:37 -0500

    last update

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ module.exports.JWT_SECRET=process.env.JWT_SECRET
 module.exports.MONGO_USER=process.env.MONGO_USER
 module.exports.MONGO_PASSWORD=process.env.MONGO_PASSWORD
 module.exports.SERVER_ROOT_URI='http://localhost:3030'
-module.exports.REDIRECT_URI_FRONT='http://localhost:3030/auth'
+module.exports.REDIRECT_URI_FRONT='http://localhost:3000/auth'
