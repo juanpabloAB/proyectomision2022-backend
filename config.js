1258d0fd7128dc9f2f62ed4835dd77e429ae246c
juanpabloAB <jp.acosta20@gmail.com>
2021-10-26 19:12:47 -0500

    final

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports.GOOGLE_CLIENT_SECRET=process.env.GOOGLE_CLIENT_SECRET
 module.exports.JWT_SECRET=process.env.JWT_SECRET
 module.exports.MONGO_USER=process.env.MONGO_USER
 module.exports.MONGO_PASSWORD=process.env.MONGO_PASSWORD
-module.exports.SERVER_ROOT_URI='http://localhost:3030'
-module.exports.REDIRECT_URI_FRONT='http://localhost:3000/auth'
+module.exports.SERVER_ROOT_URI='https://mision2022-crackcoders.herokuapp.com' //'http://localhost:3030'//https://mision2022-crackcoders.herokuapp.com/
+module.exports.REDIRECT_URI_FRONT='https://mision2022-crackcoders.herokuapp.com/auth' //'http://localhost:3030/auth'//https://mision2022-crackcoders.herokuapp.com/auth
